9196e1085db2297d1dfa615d2c8e8bc164d63e43
Jiri Laza <jiri.laza@gmail.com>
2022-06-12 10:20:41 +0200

    graphics changes

--- src/components/app/CopyButton.vue
+++ src/components/app/CopyButton.vue
@@ -58,20 +58,27 @@ export default {
 </script>
 
 <template>
-  <button @click="copy()">{{ text }}</button>
+  <button class="copy-btn" @click="copy()" :title="text">
+    <svg class="copy-btn__svg" fill="currentColor" height="22" viewBox="0 0 21 22" width="21"><path d="M14.5 0h-12C1.4 0 .5.9.5 2v14h2V2h12V0Zm3 4h-11c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2Zm0 16h-11V6h11v14Z" fill-rule="evenodd"/></svg>
+  </button>
 </template>
 
 <style scoped>
-button {
+.copy-btn {
   all: unset !important;
-  position: absolute !important;
-  right: 20px !important;
-  top: 0 !important;
+  align-items: center;
+  justify-content: center;
+  display: inline-flex !important;
   color: #fff !important;
-  padding: 8px 15px !important;
-  background: #333 !important;
+  padding: 5px !important;
   border: none !important;
   cursor: pointer !important;
-  font-size: 13px !important;
+  margin: 0 8px !important;
 }
+
+.copy-btn__svg {
+  width: 17px;
+  height: 17px;
+}
+
 </style>
--- src/views/ComponentDetailView.vue
+++ src/views/ComponentDetailView.vue
@@ -87,17 +87,43 @@ export default {
       v-show="isMarkupPanelVisible"
     >
       <div class="markup__col">
-        <CopyButton text="Copy <HTML>" :copy-data="contentHtml" />
+        <div class="markup__header">
+          <div class="markup__bookmark">
+            <svg viewBox="0 0 15 15" class="markup__icon">
+              <rect fill="#FF3C41" width="15" height="15" rx="4"></rect>
+              <path d="M10.97 2.29a.563.563 0 0 0-.495-.29.572.572 0 0 0-.488.277l-5.905 9.86a.565.565 0 0 0-.007.574c.102.18.287.289.495.289a.572.572 0 0 0 .488-.277l5.905-9.86a.565.565 0 0 0 .007-.574" fill="#28282B"></path>
+            </svg>
+            <span class="markup__title">HTML</span>
+          </div>
+          <CopyButton :copy-data="contentHtml" />
+        </div>
         <pre class="js-language-html markup__language language-html"><code>{{ contentHtml }}</code></pre>
       </div>
 
       <div class="markup__col">
-        <CopyButton text="Copy JS" :copy-data="contentJs" />
+        <div class="markup__header">
+          <div class="markup__bookmark">
+            <svg viewBox="0 0 15 15" class="markup__icon">
+              <rect fill="#0EBEFF" width="15" height="15" rx="4"></rect><path d="M8 8.366l1.845 1.065a.507.507 0 0 0 .686-.181.507.507 0 0 0-.186-.685L8.5 7.5l1.845-1.065a.507.507 0 0 0 .186-.685.507.507 0 0 0-.686-.181L8 6.634v-2.13A.507.507 0 0 0 7.5 4c-.268 0-.5.225-.5.503v2.131L5.155 5.569a.507.507 0 0 0-.686.181.507.507 0 0 0 .186.685L6.5 7.5 4.655 8.565a.507.507 0 0 0-.186.685c.134.232.445.32.686.181L7 8.366v2.13c0 .271.224.504.5.504.268 0 .5-.225.5-.503V8.366z" fill="#282828"></path>
+            </svg>
+            <span class="markup__title">CSS</span>
+          </div>
+          <CopyButton :copy-data="contentJs" />
+        </div>
         <pre class="markup__language language-javascript"><code>{{ contentJs }}</code></pre>
       </div>
 
       <div class="markup__col">
-        <CopyButton text="Copy CSS" :copy-data="contenCss" />
+        <div class="markup__header">
+          <div class="markup__bookmark">
+            <svg viewBox="0 0 15 15" class="markup__icon">
+              <rect fill="#FCD000" width="15" height="15" rx="4"></rect>
+              <path d="M6.554 3.705c0 .267-.19.496-.452.543-1.2.217-2.12 1.61-2.12 3.275 0 1.665.92 3.057 2.12 3.274a.554.554 0 0 1-.205 1.087c-1.733-.322-3.022-2.175-3.022-4.361 0-2.187 1.289-4.04 3.022-4.362a.554.554 0 0 1 .657.544zm1.892 0c0-.347.316-.607.657-.544 1.733.322 3.022 2.175 3.022 4.362 0 2.186-1.289 4.04-3.022 4.361a.554.554 0 0 1-.205-1.087c1.2-.217 2.12-1.61 2.12-3.274 0-1.665-.92-3.058-2.12-3.275a.551.551 0 0 1-.452-.543z" fill="#282828"></path>
+            </svg>
+            <span class="markup__title">JS</span>
+          </div>
+          <CopyButton :copy-data="contenCss" />
+        </div>
         <pre class="markup__language language-css"><code>{{ contenCss }}</code></pre>
       </div>
     </div>
@@ -148,6 +174,26 @@ export default {
   border-right: 3px solid #e1e1e1;
   max-height: min(250px, 50vh);
   position: relative;
+  background-color: #1d1e22;
+}
+
+.markup__header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #aaaebc;
+}
+
+.markup__bookmark {
+  display: flex;
+  padding: 9px 12px;
+  gap: 8px;
+  align-items: center;
+  font-weight: 700;
+}
+
+.markup__icon {
+  width: 17px;
 }
 
 .markup__language {
